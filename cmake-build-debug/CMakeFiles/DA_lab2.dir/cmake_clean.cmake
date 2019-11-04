6957a6cffbad87bcfca11385160c25f4ca4d7568
IllCher <50427226+IllCher@users.noreply.github.com>
2019-11-04 19:16:23 +0300

    update

---
+++ cmake-build-debug/CMakeFiles/DA_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DA_lab2.dir/main.cpp.obj"
+  "DA_lab2.exe"
+  "DA_lab2.exe.manifest"
+  "DA_lab2.pdb"
+  "libDA_lab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DA_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
