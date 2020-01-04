f5c6fddc8521e46b3260d58aab9f662339337120
IllCher <50427226+IllCher@users.noreply.github.com>
2020-01-04 18:53:53 +0300

    Add files via upload

--- benchmark/cmake-build-debug/cmake_install.cmake
+++ benchmark/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/Ilya/Desktop/Labs2course/DA_lab2/benchmark
+# Install script for directory: /home/ilya/DA_lab2-master/benchmark
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/Ilya/Desktop/Labs2course/DA_lab2/benchmark/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ilya/DA_lab2-master/benchmark/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
